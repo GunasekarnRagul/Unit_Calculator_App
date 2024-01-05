1702da4a16c3e709989e7be5f8bbb23fedb50e4a
GunasekaranRagul <raguloffic@gmail.com>
2024-01-05 14:32:46 +0530

    some Changes

--- lib/cal_properties/Length.dart
+++ lib/cal_properties/Length.dart
@@ -15,23 +15,104 @@ class Length extends StatefulWidget {
 }
 
 class _LengthState extends State<Length> {
-  void _openPickerWithCustomPickerTextStyle(BuildContext context) {
+  Map<String, String> lenghtval = {
+    '0': 'KiloMeter KM',
+    '1': 'Decimeter DM',
+    '2': 'Centimeter CM',
+    '3': 'Millimeter MM',
+    '4': 'Micrometer µm',
+    '5': 'Pico-meter PM',
+    '6': 'Nautical Mile NMI',
+    '7': 'Mile MI',
+    '8': 'Furlong FUR',
+  };
+
+  Map<String, String> lenghtvalUnit = {
+    '0': 'KM',
+    '1': 'DM',
+    '2': 'CM',
+    '3': 'MM',
+    '4': 'µm',
+    '5': 'PM',
+    '6': 'NMI',
+    '7': 'MI',
+    '8': 'FUR',
+  };
+
+  Map<String, String> lenghtvalText = {
+    '0': 'KiloMeter',
+    '1': 'Decimeter',
+    '2': 'Centimeter',
+    '3': 'Millimeter',
+    '4': 'Micrometer',
+    '5': 'Pico-meter',
+    '6': 'Nautical Mile',
+    '7': 'Mile',
+    '8': 'Furlong',
+  };
+
+  late String lengthUnitUp = 'KM';
+  late String lengthUnitDowm = 'KM';
+
+  late String lengthUnitUpText = 'KiloMeter';
+  late String lengthUnitDowmText = 'KiloMeter';
+
+  late Color lengthUpTextColor = Colors.deepOrange;
+  late Color lengthDownTextColor = Colors.white;
+
+  late String upText = 'up';
+
+  late String calUpText = '0';
+  late String calDownText = '0';
+
+  void _openPickerWithCustomPickerTextStyle(
+      BuildContext context, String place) {
     BottomPicker(
       backgroundColor: Colors.black,
       buttonSingleColor: Colors.white,
       iconColor: Colors.black,
-      items: const [
-        Text('Leonardo DiCaprio'),
-        Text('Johnny Depp'),
-        Text('Robert De Niro'),
-        Text('Tom Hardy'),
-        Text('Ben Affleck'),
+      items: [
+        Text(lenghtval['0']!),
+        Text(lenghtval['1']!),
+        Text(lenghtval['2']!),
+        Text(lenghtval['3']!),
+        Text(lenghtval['4']!),
+        Text(lenghtval['5']!),
+        Text(lenghtval['6']!),
+        Text(lenghtval['7']!),
+        Text(lenghtval['8']!),
       ],
       title: 'Select Unit',
       pickerTextStyle: const TextStyle(
         color: Colors.white,
         fontWeight: FontWeight.bold,
       ),
+      onChange: (index) {
+        // print(index);
+
+        setState(() {
+          setState(() {
+            if (place == 'up') {
+              lengthUnitUp = lenghtvalUnit[index.toString()]!;
+              lengthUnitUpText = lenghtvalText[index.toString()]!;
+            } else if (place == 'down') {
+              lengthUnitDowm = lenghtvalUnit[index.toString()]!;
+              lengthUnitDowmText = lenghtvalText[index.toString()]!;
+            }
+          });
+        });
+      },
+      onSubmit: (index) {
+        setState(() {
+          if (place == 'up') {
+            lengthUnitUp = lenghtvalUnit[index.toString()]!;
+            lengthUnitUpText = lenghtvalText[index.toString()]!;
+          } else if (place == 'down') {
+            lengthUnitDowm = lenghtvalUnit[index.toString()]!;
+            lengthUnitDowmText = lenghtvalText[index.toString()]!;
+          }
+        });
+      },
       closeIconColor: Colors.white,
     ).show(context);
   }
@@ -83,11 +164,11 @@ class _LengthState extends State<Length> {
                         Dimensions.updateDimensions(context, 'h', 10)),
                     child: Column(
                       children: [
-                        buildRow(context),
+                        buildRow(context, 'up'),
                         SizedBox(
                           height: Dimensions.updateDimensions(context, 'h', 25),
                         ),
-                        buildRow(context),
+                        buildRow(context, 'down'),
                         SizedBox(
                           height: Dimensions.updateDimensions(context, 'h', 50),
                         ),
@@ -113,9 +194,9 @@ class _LengthState extends State<Length> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                buildTextButton(context, '7', () => {}),
-                                buildTextButton(context, '8', () => {}),
-                                buildTextButton(context, '9', () => {}),
+                                buildTextButton(context, '7'),
+                                buildTextButton(context, '8'),
+                                buildTextButton(context, '9'),
                               ],
                             ),
                             SizedBox(
@@ -125,9 +206,9 @@ class _LengthState extends State<Length> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                buildTextButton(context, '4', () => {}),
-                                buildTextButton(context, '5', () => {}),
-                                buildTextButton(context, '6', () => {}),
+                                buildTextButton(context, '4'),
+                                buildTextButton(context, '5'),
+                                buildTextButton(context, '6'),
                               ],
                             ),
                             SizedBox(
@@ -137,9 +218,9 @@ class _LengthState extends State<Length> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                buildTextButton(context, '1', () => {}),
-                                buildTextButton(context, '2', () => {}),
-                                buildTextButton(context, '3', () => {}),
+                                buildTextButton(context, '1'),
+                                buildTextButton(context, '2'),
+                                buildTextButton(context, '3'),
                               ],
                             ),
                             SizedBox(
@@ -149,9 +230,9 @@ class _LengthState extends State<Length> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                buildTextButton(context, '', () => {}),
-                                buildTextButton(context, '0', () => {}),
-                                buildTextButton(context, '.', () => {}),
+                                buildTextButton(context, ''),
+                                buildTextButton(context, '0'),
+                                buildTextButton(context, '.'),
                               ],
                             ),
                           ],
@@ -163,7 +244,7 @@ class _LengthState extends State<Length> {
                         child: Column(
                           children: [
                             TextButton(
-                              onPressed: () {},
+                              onPressed: () => _allClearText(),
                               style: ElevatedButton.styleFrom(
                                 foregroundColor: Colors.white,
                                 backgroundColor: Colors.white10, // Text color
@@ -193,7 +274,7 @@ class _LengthState extends State<Length> {
                                   Dimensions.updateDimensions(context, 'h', 20),
                             ),
                             TextButton(
-                                onPressed: () {},
+                                onPressed: () => _removeLastText(),
                                 style: ElevatedButton.styleFrom(
                                   foregroundColor: Colors.white,
                                   backgroundColor: Colors.white10, // Text color
@@ -207,12 +288,9 @@ class _LengthState extends State<Length> {
                                       horizontal: 1.0,
                                       vertical: 0.0), // Adjust padding
                                 ),
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.cancel_presentation_outlined,
-                                    color: Colors.deepOrange,
-                                  ),
+                                child: const Icon(
+                                  Icons.cancel_presentation_outlined,
+                                  color: Colors.deepOrange,
                                 )),
                           ],
                         ),
@@ -226,12 +304,11 @@ class _LengthState extends State<Length> {
         ));
   }
 
-  Opacity buildTextButton(
-      BuildContext context, String text, Function function) {
+  Opacity buildTextButton(BuildContext context, String text) {
     return Opacity(
       opacity: (text.isNotEmpty) ? 1.0 : 0.0,
       child: TextButton(
-        onPressed: function as void Function(),
+        onPressed: () => _btnClicked(text),
         style: ElevatedButton.styleFrom(
           foregroundColor: Colors.white,
           backgroundColor: Colors.transparent, // Text color
@@ -255,19 +332,19 @@ class _LengthState extends State<Length> {
     );
   }
 
-  Row buildRow(BuildContext context) {
+  Row buildRow(BuildContext context, String place) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         GestureDetector(
           onTap: () {
-            _openPickerWithCustomPickerTextStyle(context);
+            _openPickerWithCustomPickerTextStyle(context, place);
           },
           child: Row(
             children: [
               RichText(
                   text: TextSpan(
-                      text: 'km'.toString(),
+                      text: (place == 'up') ? lengthUnitUp : lengthUnitDowm,
                       style: GoogleFonts.ubuntu(
                           fontSize:
                               Dimensions.updateDimensions(context, 'w', 15),
@@ -281,29 +358,86 @@ class _LengthState extends State<Length> {
             ],
           ),
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            RichText(
-                text: TextSpan(
-                    text: '1'.toString(),
-                    style: GoogleFonts.ubuntu(
-                        fontSize: Dimensions.updateDimensions(context, 'w', 25),
-                        fontWeight: FontWeight.w300,
-                        color: Colors.white))),
-            SizedBox(
-              height: Dimensions.updateDimensions(context, 'h', 5),
-            ),
-            RichText(
-                text: TextSpan(
-                    text: 'Kilometer'.toString(),
-                    style: GoogleFonts.ubuntu(
-                        fontSize: Dimensions.updateDimensions(context, 'w', 10),
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white70))),
-          ],
+        GestureDetector(
+          onTap: () {
+            _textColorChange(place);
+            (place == 'up') ? upText = 'up' : upText = 'down';
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              RichText(
+                  text: TextSpan(
+                      text: (place == 'up') ? calUpText : calDownText,
+                      style: GoogleFonts.ubuntu(
+                          fontSize:
+                              Dimensions.updateDimensions(context, 'w', 25),
+                          fontWeight: FontWeight.w300,
+                          color: (place == 'up')
+                              ? lengthUpTextColor
+                              : lengthDownTextColor))),
+              SizedBox(
+                height: Dimensions.updateDimensions(context, 'h', 5),
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: (place == 'up')
+                          ? lengthUnitUpText
+                          : lengthUnitDowmText,
+                      style: GoogleFonts.ubuntu(
+                          fontSize:
+                              Dimensions.updateDimensions(context, 'w', 10),
+                          fontWeight: FontWeight.w100,
+                          color: Colors.white70))),
+            ],
+          ),
         )
       ],
     );
+  }
+
+  void _textColorChange(String place) {
+    setState(() {
+      if (place == 'up') {
+        lengthUpTextColor = Colors.deepOrange;
+        lengthDownTextColor = Colors.white;
+      } else if (place == 'down') {
+        lengthDownTextColor = Colors.deepOrange;
+        lengthUpTextColor = Colors.white;
+      }
+    });
+  }
+
+  _btnClicked(String text) {
+    setState(() {
+      calUpText == '0' && upText == 'up' ? calUpText = '' : null;
+      calDownText == '0' && upText == 'down' ? calDownText = "" : null;
+      if (upText == 'up') {
+        calUpText += text;
+      } else if (upText == 'down') {
+        calDownText += text;
+      }
+    });
+  }
+
+  _allClearText() {
+    setState(() {
+      calUpText = '0';
+      calDownText = '0';
+    });
+  }
+
+  _removeLastText() {
+    setState(() {
+      if (upText == 'up' && calUpText.length > 1) {
+        calUpText = calUpText.substring(0, calUpText.length - 1);
+      } else if (upText == 'down' && calDownText.length > 1) {
+        calDownText = calDownText.substring(0, calDownText.length - 1);
+      } else if (upText == 'up' && calUpText.length == 1) {
+        calUpText = '0'; // Set to '0' when the length is 1
+      } else if (upText == 'down' && calDownText.length == 1) {
+        calDownText = '0'; // Set to '0' when the length is 1
+      }
+    });
   }
 }
